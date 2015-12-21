38693caef4521108013338dcc5d64c1f50124278
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-21 19:43:37 +0100

    Edit Function

--- DlgView.cpp
+++ DlgView.cpp
@@ -21,6 +21,12 @@ IMPLEMENT_DYNCREATE(DlgView, CFormView)
 BEGIN_MESSAGE_MAP(DlgView, CFormView)
 	//{{AFX_MSG_MAP(DlgView)
 	//}}AFX_MSG_MAP
+	ON_EN_CHANGE(IDC_EDIT1, &DlgView::OnEnChangeEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT1, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT2, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT3, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT4, &DlgView::OnEnKillfocusEdit1)
+
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -28,6 +34,11 @@ END_MESSAGE_MAP()
 
 DlgView::DlgView()
 	: CFormView(DlgView::IDD)
+	, left(0)
+	, right(0)
+	, top(0)
+	, bottom(0)
+	
 {
 }
 
@@ -40,6 +51,11 @@ void DlgView::DoDataExchange(CDataExchange* pDX)
 	CFormView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(DlgView)
 	//}}AFX_DATA_MAP
+	DDX_Text(pDX, IDC_EDIT1, GetDocument()->rect.left);
+	DDX_Text(pDX, IDC_EDIT2, GetDocument()->rect.top);
+	DDX_Text(pDX, IDC_EDIT3, GetDocument()->rect.right);
+	DDX_Text(pDX, IDC_EDIT4, GetDocument()->rect.bottom);
+
 }
 
 BOOL DlgView::PreCreateWindow(CREATESTRUCT& cs)
@@ -68,6 +84,9 @@ void DlgView::Dump(CDumpContext& dc) const
 	CFormView::Dump(dc);
 }
 
+
+
+
 Doc* DlgView::GetDocument() // non-debug version is inline
 {
 	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Doc)));
@@ -78,3 +97,31 @@ Doc* DlgView::GetDocument() // non-debug version is inline
 /////////////////////////////////////////////////////////////////////////////
 // DlgView message handlers
 
+
+
+
+void DlgView::OnEnChangeEdit1()
+{
+	// TODO:  If this is a RICHEDIT control, the control will not
+	// send this notification unless you override the CFormView::OnInitDialog()
+	// function and call CRichEditCtrl().SetEventMask()
+	// with the ENM_CHANGE flag ORed into the mask.
+	// TODO:  Add your control notification handler code here
+}
+
+
+void DlgView::OnEnKillfocusEdit1()
+{
+	// TODO: Add your control notification handler code here
+	UpdateData(true);
+	GetDocument()->UpdateAllViews(this);
+}
+
+
+
+
+void DlgView::OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/)
+{
+	// TODO: Add your specialized code here and/or call the base class
+	UpdateData(false);
+}
