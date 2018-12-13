6a38779094b389196354d487d46380e906d3f113
Hrvoje Kuhar <hrkuhar@vsite.hr>
2018-12-13 13:15:28 +0100

    drawing ellipse

--- DlgView.h
+++ DlgView.h
@@ -53,6 +53,16 @@ protected:
 	//{{AFX_MSG(DlgView)
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
+public:
+	int left;
+	int top;
+	int right;
+	int bottom;
+	afx_msg void OnEnKillfocusEdit1();
+	afx_msg void OnEnKillfocusEdit2();
+	afx_msg void OnEnKillfocusEdit3();
+	afx_msg void OnEnKillfocusEdit4();
+	virtual void OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/);
 };
 
 #ifndef _DEBUG  // debug version in DlgView.cpp
