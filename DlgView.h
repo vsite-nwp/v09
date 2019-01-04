da4237ab542e192179962e514ef0a5f8c05eb32e
Luka Radenovic <luka@artkod.com>
2019-01-04 10:40:36 +0000

    Dovrsena vjezba 9

--- DlgView.h
+++ DlgView.h
@@ -53,6 +53,9 @@ protected:
 	//{{AFX_MSG(DlgView)
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnKillfocus();
+	void OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/);
 };
 
 #ifndef _DEBUG  // debug version in DlgView.cpp
