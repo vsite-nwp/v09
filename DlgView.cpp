64535f278017e23c410c4a66f44c0267c3bfb3c5
fisamodo <56238728+fisamodo@users.noreply.github.com>
2020-01-16 23:13:23 +0100

    Add files via upload

--- DlgView.cpp
+++ DlgView.cpp
@@ -1,5 +1,4 @@
-// DlgView.cpp : implementation of the DlgView class
-//
+
 
 #include "stdafx.h"
 #include "v9.h"
@@ -13,18 +12,16 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-/////////////////////////////////////////////////////////////////////////////
-// DlgView
-
-IMPLEMENT_DYNCREATE(DlgView, CFormView)
+IMPLEMENT_DYNCREATE(DlgView, CFormView) 
 
 BEGIN_MESSAGE_MAP(DlgView, CFormView)
-	//{{AFX_MSG_MAP(DlgView)
-	//}}AFX_MSG_MAP
+	ON_EN_KILLFOCUS(IDC_EDIT1, &DlgView::OnEnKillfocusEdit)
+	ON_EN_KILLFOCUS(IDC_EDIT2, &DlgView::OnEnKillfocusEdit)
+	ON_EN_KILLFOCUS(IDC_EDIT3, &DlgView::OnEnKillfocusEdit)
+	ON_EN_KILLFOCUS(IDC_EDIT4, &DlgView::OnEnKillfocusEdit)
+
 END_MESSAGE_MAP()
 
-/////////////////////////////////////////////////////////////////////////////
-// DlgView construction/destruction
 
 DlgView::DlgView()
 	: CFormView(DlgView::IDD)
@@ -38,8 +35,11 @@ DlgView::~DlgView()
 void DlgView::DoDataExchange(CDataExchange* pDX)
 {
 	CFormView::DoDataExchange(pDX);
-	//{{AFX_DATA_MAP(DlgView)
-	//}}AFX_DATA_MAP
+	DDX_Text(pDX, IDC_EDIT1, GetDocument()->rct.left);
+	DDX_Text(pDX, IDC_EDIT2, GetDocument()->rct.top);
+	DDX_Text(pDX, IDC_EDIT3, GetDocument()->rct.right);
+	DDX_Text(pDX, IDC_EDIT4, GetDocument()->rct.bottom);
+
 }
 
 BOOL DlgView::PreCreateWindow(CREATESTRUCT& cs)
@@ -54,8 +54,6 @@ void DlgView::OnInitialUpdate()
 
 }
 
-/////////////////////////////////////////////////////////////////////////////
-// DlgView diagnostics
 
 #ifdef _DEBUG
 void DlgView::AssertValid() const
@@ -68,13 +66,18 @@ void DlgView::Dump(CDumpContext& dc) const
 	CFormView::Dump(dc);
 }
 
-Doc* DlgView::GetDocument() // non-debug version is inline
+Doc* DlgView::GetDocument() 
 {
 	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(Doc)));
 	return (Doc*)m_pDocument;
 }
-#endif //_DEBUG
+#endif 
 
-/////////////////////////////////////////////////////////////////////////////
-// DlgView message handlers
+void DlgView::OnEnKillfocusEdit() {
+	UpdateData(true);
+	GetDocument()->UpdateAllViews(this);
+}
 
+void DlgView::OnUpdate(CView*, LPARAM, CObject*) {
+	UpdateData(false);
+}
