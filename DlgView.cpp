df882b53456821ece9799c38ba2253c3fb45e037
Jasmin Husić <jhsc1988@gmail.com>
2021-05-17 10:08:22 +0200

    v09 done

--- DlgView.cpp
+++ DlgView.cpp
@@ -21,6 +21,11 @@ IMPLEMENT_DYNCREATE(DlgView, CFormView)
 BEGIN_MESSAGE_MAP(DlgView, CFormView)
 	//{{AFX_MSG_MAP(DlgView)
 	//}}AFX_MSG_MAP
+	ON_EN_KILLFOCUS(IDC_EDIT1, &DlgView::OnKillfocus)
+	ON_EN_KILLFOCUS(IDC_EDIT2, &DlgView::OnKillfocus)
+	ON_EN_KILLFOCUS(IDC_EDIT3, &DlgView::OnKillfocus)
+	ON_EN_KILLFOCUS(IDC_EDIT4, &DlgView::OnKillfocus)
+	ON_WM_KILLFOCUS()
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -40,6 +45,10 @@ void DlgView::DoDataExchange(CDataExchange* pDX)
 	CFormView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(DlgView)
 	//}}AFX_DATA_MAP
+	DDX_Text(pDX, IDC_EDIT1, GetDocument()->rect.left);
+	DDX_Text(pDX, IDC_EDIT2, GetDocument()->rect.top);
+	DDX_Text(pDX, IDC_EDIT3, GetDocument()->rect.right);
+	DDX_Text(pDX, IDC_EDIT4, GetDocument()->rect.bottom);
 }
 
 BOOL DlgView::PreCreateWindow(CREATESTRUCT& cs)
@@ -77,4 +86,18 @@ Doc* DlgView::GetDocument() // non-debug version is inline
 
 /////////////////////////////////////////////////////////////////////////////
 // DlgView message handlers
+
+
+
+void DlgView::OnKillfocus()
+{
+	UpdateData(true);
+	GetDocument()->UpdateAllViews(this);
+}
+
+
+void DlgView::OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/)
+{
+	UpdateData(false);
+}
 
