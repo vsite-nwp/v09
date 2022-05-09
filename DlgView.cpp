0694c71c7134c477c57dcb714e5d775d9780bacc
robilovric <robilovric7@gmail.com>
2022-05-09 22:43:54 +0200

    Resolve MDI demo

--- DlgView.cpp
+++ DlgView.cpp
@@ -21,6 +21,10 @@ IMPLEMENT_DYNCREATE(DlgView, CFormView)
 BEGIN_MESSAGE_MAP(DlgView, CFormView)
 	//{{AFX_MSG_MAP(DlgView)
 	//}}AFX_MSG_MAP
+	ON_EN_KILLFOCUS(IDC_EDIT1, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT2, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT3, &DlgView::OnEnKillfocusEdit1)
+	ON_EN_KILLFOCUS(IDC_EDIT4, &DlgView::OnEnKillfocusEdit1)
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -40,6 +44,10 @@ void DlgView::DoDataExchange(CDataExchange* pDX)
 	CFormView::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(DlgView)
 	//}}AFX_DATA_MAP
+	DDX_Text(pDX, IDC_EDIT1, GetDocument()->rect.left);
+	DDX_Text(pDX, IDC_EDIT2, GetDocument()->rect.top);
+	DDX_Text(pDX, IDC_EDIT3, GetDocument()->rect.right);
+	DDX_Text(pDX, IDC_EDIT4, GetDocument()->rect.bottom);
 }
 
 BOOL DlgView::PreCreateWindow(CREATESTRUCT& cs)
@@ -78,3 +86,18 @@ Doc* DlgView::GetDocument() // non-debug version is inline
 /////////////////////////////////////////////////////////////////////////////
 // DlgView message handlers
 
+
+
+void DlgView::OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/)
+{
+	//: Add your specialized code here and/or call the base class
+	UpdateData(false);
+}
+
+
+void DlgView::OnEnKillfocusEdit1()
+{
+	//: Add your control notification handler code here
+	UpdateData(true);
+	GetDocument()->UpdateAllViews(this);
+}
--- DlgView.h
+++ DlgView.h
@@ -53,6 +53,9 @@ protected:
 	//{{AFX_MSG(DlgView)
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
+	virtual void OnUpdate(CView* /*pSender*/, LPARAM /*lHint*/, CObject* /*pHint*/);
+public:
+	afx_msg void OnEnKillfocusEdit1();
 };
 
 #ifndef _DEBUG  // debug version in DlgView.cpp
